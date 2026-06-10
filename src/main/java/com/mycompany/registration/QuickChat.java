c04c08f532f15e7539ab7b71ae8dc77c6a0759c9
Akanyang99 <akanyangc1@gmail.com>
2026-06-10 15:11:19 +0200

    Update QuickChat.java
    updated QuickChats

--- src/main/java/com/mycompany/registration/QuickChat.java
+++ src/main/java/com/mycompany/registration/QuickChat.java
@@ -46,5 +46,76 @@ public class QuickChat {
         }
 
         System.out.println("\nTotal messages sent: " + Message.returnTotalMessages());
+        System.out.println("\nTotal messages sent: "
+        + Message.returnTotalMessages());
+
+char option;
+
+do {
+
+    System.out.println("\nSTORED MESSAGES MENU");
+    System.out.println("a) Display sender and recipient of all stored messages");
+    System.out.println("b) Display the longest stored message");
+    System.out.println("c) Search for a message by Message ID");
+    System.out.println("d) Search for all messages for a particular recipient");
+    System.out.println("e) Delete a message using its Message Hash");
+    System.out.println("f) Display full report of all stored messages");
+    System.out.println("g) Return to CloudChat Menu");
+    System.out.print("Choose an option: ");
+
+    option = input.nextLine().toLowerCase().charAt(0);
+
+    switch (option) {
+
+        case 'a':
+
+            Message.displayStoredMessages();
+            break;
+
+        case 'b':
+
+            Message.displayLongestStoredMessage();
+            break;
+
+        case 'c':
+
+            System.out.print("Enter Message ID: ");
+            String id = input.nextLine();
+
+            Message.searchMessageID(id);
+            break;
+
+        case 'd':
+
+            System.out.print("Enter Recipient Number: ");
+            String recipient = input.nextLine();
+
+            Message.searchRecipient(recipient);
+            break;
+
+        case 'e':
+
+            System.out.print("Enter Message Hash: ");
+            String hash = input.nextLine();
+
+            Message.deleteMessage(hash);
+            break;
+
+        case 'f':
+
+            Message.displayReport();
+            break;
+
+        case 'g':
+
+            System.out.println("Returning to CloudChat Menu...");
+            break;
+
+        default:
+
+            System.out.println("Invalid option.");
+    }
+
+} while (option != 'g');
     }
 }
